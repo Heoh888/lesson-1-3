62d6e8f46b851d048e377346a24f84e8e89662d7
alexfit <heoh888@ya.ru>
2020-12-20 16:09:53 +0300

    add struct

--- lesson-1-3/main.swift
+++ lesson-1-3/main.swift
@@ -1,11 +1,55 @@
-//
-//  main.swift
-//  lesson-1-3
-//
-//  Created by MacBook on 20.12.2020.
-//
+/*
+ 1. Описать несколько структур – любой легковой автомобиль SportCar и любой грузовик TrunkCar.
 
+ 2. Структуры должны содержать .
+
+ 3. Описать перечисление с возможными действиями с автомобилем: запустить/заглушить двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
+
+ 4. Добавить в структуры метод с одним аргументом типа перечисления, который будет менять свойства структуры в зависимости от действия.
+
+ 5. Инициализировать несколько экземпляров структур. Применить к ним различные действия.
+
+ 6. Вывести значения свойств экземпляров в консоль.
+ */
 import Foundation
 
-print("Hello, World!")
+enum WindowsStatus {
+    case open, closed
+}
+
+enum EngineStatus {
+    case started, muted
+}
+
+struct SportCar {
+    var carBrand: String
+    let year: Int
+    let trunkVolume: Int
+    var engine: EngineStatus
+    var windows: WindowsStatus
+    var filledTrunkVolume: Bool
+}
+
+struct TrunkCar {
+    var carBrand: String
+    let year: Int
+    let bodyVolume: Int
+    var engine: EngineStatus
+    var windows: WindowsStatus
+    var filledBodyVolume: Bool
+}
+
+var lotus = SportCar(carBrand: "Lotus", year: 2020, trunkVolume: 200, engine: .started, windows: .closed, filledTrunkVolume: false)
+
+var ferrari = SportCar(carBrand: "Ferrari", year: 2020, trunkVolume: 174, engine: .muted, windows: .open, filledTrunkVolume: true)
+
+var volvo = TrunkCar(carBrand: "Volvo", year: 2019, bodyVolume: 74, engine: .started, windows: .open, filledBodyVolume: true)
+
+var Kamaz = TrunkCar(carBrand: "Kamaz", year: 2016, bodyVolume: 65, engine: .muted, windows: .closed, filledBodyVolume: false)
+
+
+print(lotus)
+print(ferrari)
+print(volvo)
+print(Kamaz)
 
